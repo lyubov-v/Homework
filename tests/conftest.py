98f59a3cecb0bccb6a2515878cc2ea7ece745b3d
Lyubov_Rodionova <09rlv1992o1364@gmal.com>
2024-11-17 14:33:42 +0300

    add fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+@pytest.fixture
+def bank_data():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+@pytest.fixture
+def incorrect_date():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "03.07.2019T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "12.09.2018T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21"},
+    ]
