85af49f3909d07cf5f83e66baf0ff2e47ab118e5
Lyubov_Rodionova <09rlv1992o1364@gmal.com>
2025-06-07 20:57:30 +0300

    create test_decorators

--- src/decorators.py
+++ src/decorators.py
@@ -4,19 +4,22 @@ from functools import wraps
 def log(filename):
     def wrapper(function):
         @wraps(function)
-        def recording(*args, **kwargs):
-            result = function(*args,**kwargs)
+        def recording(*args,**kwargs):
             try:
-                if filename == None or filename == "":
-                    print(f"{function.__name__} OK")
-                with open(filename, "w") as file:
-                    file.write(f"{function.__name__} OK\n")
+                result = function(*args,**kwargs)
             except Exception as e:
-                if filename == None or filename == "":
-                    print(f"{function.__name__} error {e} arguments: {args}, {kwargs}")
-
-                with open(filename, "w") as file:
-                    file.write(f"{function.__name__} error {e} arguments: {args}, {kwargs}")
+                result = None
+                if filename == None:
+                    print(f"{function.__name__} error {e}, arguments: {args}, {kwargs}")
+                else:
+                    with open(filename, "a") as file:
+                        file.write(f"{function.__name__} error {e}, arguments: {args}, {kwargs}")
+            if result:
+                if filename == None:
+                    print(f"{function.__name__} OK")
+                else:
+                    with open(filename, "a") as file:
+                        file.write(f"{function.__name__} OK\n")
             return result
         return recording
     return wrapper
@@ -28,5 +31,13 @@ def get_date(data_file: str) -> str:
     return f"{data_file[8:10]}.{data_file[5:7]}.{data_file[:4]}"
 
 get_date("2019-07-03T18:35:29.512364")
+
+
+@log(filename=None)
+def positive_summ(a: int, b: int) -> int:
+    if a >0 and b > 0:
+        result = a + b
+    return result
+positive_summ(-1,8)
 
 
